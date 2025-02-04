aaf67d92517f800a0bd63baa5b203cbcb4f50c3a
JacobTweeten <jacobtweeten@outlook.com>
2025-02-04 18:29:07 -0500

    Adding super oil, and bat.
    Fixed the bug again

--- KeepOn/Collectables/super_oil_can.gd
+++ KeepOn/Collectables/super_oil_can.gd
@@ -1,7 +1,7 @@
-class_name Oil
+class_name SuperOil
 extends Node2D
 
-@export var oilAmount: float = 25
+@export var oilAmount: float = 100
 func _on_oil_pick_up_body_entered(body: Node2D) -> void:
 	if body is Player:
 		body.health += oilAmount
