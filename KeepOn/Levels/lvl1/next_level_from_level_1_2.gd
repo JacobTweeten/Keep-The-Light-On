ffcbf1b175d0dc2fcb2ccd93ca4f4a23d6491038
JacobTweeten <jacobtweeten@outlook.com>
2025-02-13 19:32:10 -0500

    Work

--- KeepOn/Levels/lvl1/next_level_from_level_1_2.gd
+++ KeepOn/Levels/lvl1/next_level_from_level_1_2.gd
@@ -8,6 +8,6 @@ func _on_body_entered(body: Node2D) -> void:
 		var current_scene_file = get_tree().current_scene.scene_file_path
 		var next_level_number = current_scene_file.to_int() + 1
 		
-		var next_level_path = "res://Levels/testLevel2.tscn"
+		var next_level_path = "res://Levels/Level_2.tscn"
 		get_tree().change_scene_to_file(next_level_path)
 		
--- KeepOn/Levels/titleScreen/main_menu.gd
+++ KeepOn/Levels/titleScreen/main_menu.gd
@@ -5,5 +5,5 @@ extends Node2D
 
 func _on_button_pressed() -> void:
 	print("button Pressed")
-	SceneTransition.change_scene("res://Levels/testLevel.tscn")
+	SceneTransition.change_scene("res://Levels/level_0_and_1.tscn")
 	Global.score = 0
